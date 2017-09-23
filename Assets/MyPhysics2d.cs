a67938b214fcb6f956fc7bee3a3857a073e27f0a
Samuel <samuel0808@gmail.com>
2017-09-23 10:06:35 +0200

    Collision with the planet and jumping now working for the player

---
+++ Assets/MyPhysics2d.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Physics2d : MonoBehaviour {
+	
+	public Vector2 gravity = new Vector2(0,-5);
+
+	// Use this for initialization
+	void Awake () {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+	}
+}
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
+	Vector2 jumpVelocity = new Vector2(0,3);	
+	Vector2 velocity = Vector2.zero;
 
-	bool didJump = false;
+	public LayerMask RayMask;
+
+	public Transform TargetPlanet;
+
+	public bool didJump = false;
+	private Rigidbody2D body;
+
+
 	// Use this for initialization
 	void Start () {
-		
+		body = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
@@ -17,4 +26,43 @@ public class PlayerMovement : MonoBehaviour {
 			didJump = true;
 		}
 	}
+
+	void FixedUpdate(){
+
+		float gravity = 2.0f;
+		
+		Vector2 vectorToPlanet = TargetPlanet.position - transform.position;
+		Vector2 directionToPlanet = vectorToPlanet.normalized;
+		float distanceToPlanet = vectorToPlanet.magnitude;
+
+		velocity += directionToPlanet * gravity * Time.deltaTime;
+		Debug.DrawRay(transform.position, vectorToPlanet, Color.red);
+
+		if(didJump == true){
+
+			didJump = false;
+			velocity = jumpVelocity;
+		}	
+
+		float raycastDistance = 0.5f + velocity.magnitude * Time.deltaTime;
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, velocity.normalized, raycastDistance, RayMask);
+		Debug.DrawRay(transform.position, velocity.normalized  * (hit ? hit.distance : raycastDistance), Color.yellow);
+	
+		if(hit){
+			velocity = velocity.normalized * (hit.distance - 0.5f) * Time.deltaTime;
+		}
+
+		body.position += velocity * Time.deltaTime;
+		
+	}
+
+	void OnCollisionStay2D(Collision2D col){
+		print("collision function");
+		
+//		velocity = Vector2.zero;
+//		body.position += velocity * Time.deltaTime;
+
+	}
+
+
 }
