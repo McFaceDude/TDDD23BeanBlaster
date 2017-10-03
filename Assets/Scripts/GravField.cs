4d183a15368dc30d317ff87d2ae625e679622d0b
Samuel <samuel0808@gmail.com>
2017-10-03 23:51:37 +0200

    Moved two script files

---
+++ Assets/Scripts/GravField.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravField : MonoBehaviour {
+
+	public float gravity = 40f;
+	public float planetFriction = 0.9f; 
+	public float atmosphereFriction = 0.95f;
+	
+}
---
+++ Assets/Scripts/GravObject.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Has the positions of the planet and sets all the vectors for the obejcts effected by gravity and the calls the addVelocity method 
+
+public class GravObject : MonoBehaviour {
+
+	GravField currentGravField = null;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//When a obejct enters a gravField, get the gravField
+	void OnTriggerEnter2D(Collider2D gravField){
+		print("Collided with gravField");
+		GravField field = gravField.GetComponent<GravField>();
+		GetComponentInParent<PlayerMovement>().SetGravitySource(field); 
+	}
+
+	void OnTriggerExit2D(Collider2D gravField){
+		print("Leaving");
+		
+		GetComponentInParent<PlayerMovement>().SetGravitySource(null); 
+	}
+}
